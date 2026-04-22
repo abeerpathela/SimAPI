762540eed22bf3fdfdae155b694a947e4468a413
abeerpathela <pathelaabeer@gmail.com>
2026-04-22 19:17:14 +0530

    App Deployment fix_1

--- mobile_app/lib/config/api_constants.dart
+++ mobile_app/lib/config/api_constants.dart
@@ -1,11 +1,4 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class ApiConstants {
-  static String get baseUrl {
-    // Priority: .env > fallback for Android Emulator > fallback for local
-    return dotenv.env['API_BASE_URL'] ?? 'http://10.0.2.2:4000';
-  }
-
-  // Add other constants here if needed
+  static const String baseUrl = 'https://simapi-tzyo.onrender.com';
   static const String socketPath = '/socket.io/';
 }
